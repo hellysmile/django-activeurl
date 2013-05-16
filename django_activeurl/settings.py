886f2bd3e84e746554fbb22e82265b88d873e414
hellysmile <hellysmile@gmail.com>
2013-05-16 17:20:32 +0300

    PEP8

--- django_activeurl/settings.py
+++ django_activeurl/settings.py
@@ -1,9 +1,19 @@
 from django.conf import settings
 
 
-CACHE_ACTIVE_URL = getattr(settings, 'CACHE_ACTIVE_URL', True)
+CACHE_ACTIVE_URL = getattr(
+    settings, 'CACHE_ACTIVE_URL', True
+)
+
 CACHE_ACTIVE_URL_TIMEOUT = getattr(
-    settings, 'CACHE_ACTIVE_URL_TIMEOUT', 60 * 60 * 24)
+    settings, 'CACHE_ACTIVE_URL_TIMEOUT', 60 * 60 * 24
+)
+
 CACHE_ACTIVE_URL_PREFIX = getattr(
-    settings, 'CACHE_ACTIVE_URL_PREFIX', 'django_activeurl.')
-DEFAULT_KWARGS = {'css_class': 'active', 'parent_tag': 'li'}
+    settings, 'CACHE_ACTIVE_URL_PREFIX', 'django_activeurl.'
+)
+
+DEFAULT_KWARGS = {
+    'css_class': 'active',
+    'parent_tag': 'li'
+}
