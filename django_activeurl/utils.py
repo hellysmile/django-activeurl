bbdee47adc4f5bfd0da31c8ee15f38e0894924be
orf <tom@tomforb.es>
2013-11-19 18:19:10 +0000

    Make / be highlighted in menu mode
    If both full_path and href are "/" then mark it as active, otherwise it is always skipped.

--- django_activeurl/utils.py
+++ django_activeurl/utils.py
@@ -54,6 +54,8 @@ def check_active(url, element, full_path, css_class, menu):
             if href != '/':
                 # start with logic
                 logic = full_path.startswith(href)
+            elif href == "/" and full_path == "/":
+                logic = True
             else:
                 logic = False
         else:
