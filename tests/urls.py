1a45062ecc93e3bf9dcc5297b7d62ee25bef53f2
Tony Narlock <tony@git-pull.com>
2017-07-27 14:15:36 -0500

    update urls for django 1.8+ in tests

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-try:
-    from django.conf.urls.defaults import patterns, url
-except ImportError:
-    from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -21,8 +18,7 @@ def jinja_template_view(request):
     return render(request, 'jinja.html', {})
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', view),
     url(r'^template/django/$', djnago_template_view),
     url(r'^template/jinja/$', jinja_template_view),
@@ -31,4 +27,4 @@ urlpatterns = patterns(
     url(r'^menu/submenu/$', view),
     url(r'^страница/$', view),
     url(r'^другая_страница/$', view, name='non-ascii-reverse')
-)
+]
