583af59160c72e2c9737e0e5e53efae9c81338f4
hellysmile <hellysmile@gmail.com>
2017-07-28 14:07:12 +0300

    Django 2+ support for tests

--- tests/jinja_config.py
+++ tests/jinja_config.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.core.urlresolvers import reverse
 from django_activeurl.ext.django_jinja import ActiveUrl
 from jinja2 import Environment
+
+try:
+    # django < 2
+    from django.core.urlresolvers import reverse
+except ImportError:
+    # django > 2
+    from django.urls import reverse
 
 
 def environment(**options):
